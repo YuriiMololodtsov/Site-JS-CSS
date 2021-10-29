9a03f83c9615fcdbb2e840a6c5f70e0a670516c1
Yurii <yuriimolodtsov@gmail.com>
2021-10-29 13:29:22 +0300

    up read more content

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,10 @@ let themeButton = document.querySelector(".read-more__btn");
 themeButton.onclick = function () {
   page.classList.toggle("active");
 };
+
+const contentReadMoreBtn = document.querySelector(".content__readmore-btn");
+const contentText3 = document.querySelector(".content__text-3");
+
+contentReadMoreBtn.addEventListener("click", function () {
+  contentText3.classList.toggle("--active");
+});
